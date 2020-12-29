a27b43190ff49594c63dc86220b4d90523767ab2
nekowex <nekowex@starpony.com>
2020-12-29 19:39:37 +0500

    add realtime song status update

--- app/playlistsmanager/handlers.py
+++ app/playlistsmanager/handlers.py
@@ -3,16 +3,16 @@ from aiohttp import web
 from app import mpdservices
 
 # Текущий список воспроизведения
-def get_current_playlist(request):
+async def get_current_playlist(request):
     return web.Response(status=200)
     # return web.json_response(services.current_playlist(1))
 
-def set_playing_track(request):
+async def set_playing_track(request):
     track_id = int(request.match_info['id'])
     # mpdservices.play_track_by_number(track_id)
     return web.Response(status=200)
 
-def rm_track(request):
+async def rm_track(request):
     # track_id = int(request.match_info['id'])
     # print(f"Юзер потребовал удалить песню с id {track_id}")
     # services.remove_track_by_number(track_id)
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,15 @@ from .playlistsmanager import handlers as mm_handlers
 
 # Базовый маршрутизатор запросов
 routes = [
+    # Подключение к плееру
     web.get('/api/player/ws', player_handlers.ws_handler),
+    # Урлы работы с файлами
+    #web.get('/api/files/') Отдать список файлов и папок в директории
+    #web.get('api/files/update') Обновить БД mpd
+    #web.get('api/files/toplaylist') Добавить в плейлист
+    #web.get('api/files/toplaying') Добавить в текущий список воспроизведения
+    #web.get('api/files/play') Воспроизвести выбранный файл 
+    # Урлы работы с плейлистами
     web.get('/api/playlists/current', mm_handlers.get_current_playlist),
     web.get('/api/playlists/current/play/{id}', mm_handlers.set_playing_track),
     web.get('/api/playlists/current/remove/{id}', mm_handlers.rm_track)
